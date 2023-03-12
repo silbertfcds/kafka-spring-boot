64a9ccb21d106192cd19408fc342a6417486c4ef
Silbert Firmino <silbert.fcds@hotmai.com>
2023-03-12 13:26:14 -0300

    Adicionando grupos de consumidores e definindo consumo de topico definindo partição especifica.

--- str-consumer/src/main/java/com/firmino/strconsumer/listeners/StringConsumerKafkaListener.java
+++ str-consumer/src/main/java/com/firmino/strconsumer/listeners/StringConsumerKafkaListener.java
@@ -2,14 +2,27 @@ package com.firmino.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StringConsumerKafkaListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message){
-       log.info("Receive message: {}", message);
+    @KafkaListener(groupId = "group-1", topicPartitions = {@TopicPartition(topic = "str-topic",
+            partitions = {"0"})}, containerFactory = "strContainerFactory")
+    public void listenerOne(String message) {
+        log.info("Listener One ::: Receive message: {}", message);
+    }
+
+    @KafkaListener(groupId = "group-1", topicPartitions = {@TopicPartition(topic = "str-topic",
+            partitions = {"1"})}, containerFactory = "strContainerFactory")
+    public void listenerTwo(String message) {
+        log.info("Listener Two ::: Receive message: {}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str", containerFactory = "strContainerFactory")
+    public void history(String message) {
+        log.info("history ::: Receive message: {}", message);
     }
 }
